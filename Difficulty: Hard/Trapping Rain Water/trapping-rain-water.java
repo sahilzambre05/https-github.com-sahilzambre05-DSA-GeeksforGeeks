bfc0faaafc9e9ec35117370617d8e4d103bdb302
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-05-27 10:50:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
@@ -1,33 +1,28 @@
 class Solution {
     public int maxWater(int height[]) {
         // code here
-        int leftmax=0;
-        int rightmax=0;
-        int n = height.length;
-        int count=0;
-        int l = 0;
-        int r = n-1;
-        while(l<=r){
-            if(height[l]<=height[r]){
-                if(leftmax<=height[l]){
-                    leftmax = height[l];
-                }else{
-                    count+=leftmax-height[l];
-                }
+        int leftMax=0;
+        int rightMax=0;
+        int l=0;
+        int r=height.length-1;
+        int ans=0;
+        while(l<r){
+            if(leftMax<height[l]){
+                leftMax=height[l];
+            }
+            if(rightMax<height[r]){
+                rightMax=height[r];
+            }
+            if(leftMax<rightMax){
+                ans+= leftMax - height[l];
                 l++;
             }else{
-                if(rightmax<=height[r]){
-                    rightmax = height[r];
-                }else{
-                    count+=rightmax-height[r];
-                }
+                ans+= rightMax - height[r];
                 r--;
 
             }
-
         }
-        return count;
-        
+        return ans;
         
     }
 }
