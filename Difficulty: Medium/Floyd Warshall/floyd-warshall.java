adbebaf2f6b18c4e81cde292cb9b2000f5d5dfc1
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-07-12 20:24:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void floydWarshall(int[][] dist) {
+        // Code here
+        int n = dist.length;
+        int INF = 100000000;
+        
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dist[i][k]!=INF && dist[k][j]!=INF){
+                        dist[i][j] = Math.min(dist[i][j],dist[i][k]+dist[k][j]);
+                    }
+                }
+            }
+        }
+        
+        
+        
+        
+    
+        
+    }
+}
