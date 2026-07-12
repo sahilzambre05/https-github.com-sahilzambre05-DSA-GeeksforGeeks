327305db1356501effa7dc06f28db515134cdf62
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-07-12 22:22:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] bellmanFord(int V, int[][] edges, int src) {
+        // code here
+        int[] dist = new int[V];
+        Arrays.fill(dist,(int)1e8);
+        dist[src] = 0;
+        for(int i=0;i<V-1;i++){
+            for(int edge[] : edges){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                if(dist[u]!=(int)1e8 && dist[u]+w<dist[v]){
+                    dist[v] = dist[u] + w;
+                }
+            }
+            
+        }
+        for(int edge[] : edges){
+                int u = edge[0];
+                int v = edge[1];
+                int w = edge[2];
+                if(dist[u]!=(int)1e8 && dist[u]+w<dist[v]){
+                    return new int[]{-1};
+                }
+        }
+        return dist;
+    }
+}
