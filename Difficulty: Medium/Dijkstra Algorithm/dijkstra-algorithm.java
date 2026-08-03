0bf504b3c7767a237d4e2a4d1ebf0d079c86fc41
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-08-03 22:28:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
@@ -1,9 +1,10 @@
 class Solution {
     public int[] dijkstra(int V, int[][] edges, int src) {
         // code here
-        ArrayList<ArrayList<ArrayList<Integer>>> adjList = new ArrayList<>();
+        ArrayList<ArrayList<ArrayList<Integer>>> list = new ArrayList<>();
+        
         for(int i=0;i<V;i++){
-            adjList.add(new ArrayList<>());
+            list.add(new ArrayList<>());
         }
         
         for(int[] edge : edges){
@@ -14,34 +15,34 @@ class Solution {
             ArrayList<Integer> temp1 = new ArrayList<>();
             temp1.add(v);
             temp1.add(w);
-            adjList.get(u).add(temp1);
+            list.get(u).add(temp1);
             
             ArrayList<Integer> temp2 = new ArrayList<>();
             temp2.add(u);
             temp2.add(w);
-            adjList.get(v).add(temp2);
+            list.get(v).add(temp2);
         }
         
+        int[] dist = new int[V];
         
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
-        int[] dist = new int[V];
         Arrays.fill(dist,Integer.MAX_VALUE);
         dist[src]=0;
         pq.offer(new int[]{src,0});
         while(!pq.isEmpty()){
-            int node[] = pq.poll();
+            int[] node = pq.poll();
             int u = node[0];
             int d = node[1];
-            if(dist[u]<d) continue;
-            for(ArrayList<Integer> arr : adjList.get(u)){
-                int v = arr.get(0);
-                int w = arr.get(1);
-                if(dist[u]+w < dist[v]){
+            for(ArrayList<Integer> a : list.get(u)){
+                int v = a.get(0);
+                int w = a.get(1);
+                if(dist[u]+w<dist[v]){
                     dist[v] = dist[u]+w;
                     pq.offer(new int[]{v,dist[v]});
                 }
                 
             }
+            
         }
         return dist;
     }
